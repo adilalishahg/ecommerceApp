9dbcbcb88aeaa79371e5e22b10e11ef23bf14516
adilalishahg <adilalishahg@gmail.com>
2024-03-29 18:49:47 -0700

    [Routes][Navigation][Working On]

---
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import {AiOutlineHome,AiOutlineShopping,AiOutlineLogin,AiOutlineUserAdd,AiOutlineShoppingCart} from "react-icons/ai"
+import {FaHeart} from "react-icons/fa"
+import {Link,useNavigate} from "react-router-dom"
+import "./Navigation.css"
+
+
+ 
+const Navigation = () => {
+  return (
+    <div>Navigation</div>
+  )
+}
+
+export default Navigation
