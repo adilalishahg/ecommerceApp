ab5746ebec2cd024f18c10d6cd1f5921719be73d
adilalishahg <adilalishahg@gmail.com>
2024-03-30 18:02:31 -0700

    [Redux][Login]

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -3,12 +3,76 @@ import {AiOutlineHome,AiOutlineShopping,AiOutlineLogin,AiOutlineUserAdd,AiOutlin
 import {FaHeart} from "react-icons/fa"
 import {Link,useNavigate} from "react-router-dom"
 import "./Navigation.css"
-
+import {useSelector,useDispatch} from "react-redux"
+import {useLoginMutation} from "../../redux/api/usersApiSlice"
+import {logout} from "../../redux/features/auth/authSlice"
 
  
 const Navigation = () => {
+  const {userInfo} = useSelector(state=>state.auth)
+  const [dropdownOpen,setDropDownOpen]=useState(false)
+  const [showSidebar,setShowSidebar]=useState(false)
+  const toggleDropDown=()=>{
+    setDropDownOpen(!dropdownOpen)
+  }
+  const toggleSidebar=()=>{
+    setShowSidebar(!showSidebar)
+  }
+  const closeSidebar=()=>{
+    setShowSidebar(false)
+  }
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [logoutApiCall] = useLoginMutation()
+const logoutHandler = async()=>{
+  try {
+    await logoutApiCall().unwrap()
+    dispatch(logout())
+    navigate('/login')
+  } catch (error) {
+    console.log(error)
+  }
+}
   return (
-    <div>Navigation</div>
+    <div style={{ zIndex:999 }} id='navigation-container'
+        className={`${showSidebar?"hidden":"flex"} lg:flex xl:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-black w-[4%] hover:w-[15%] h-[100vh] fixed`}>
+          <div className="flex flex-col justify-center space-y-4">
+            <Link to="/" className="flex items-center transition-transform transform hover:translate-x-2"><AiOutlineHome size={26} className="mr-2 mt-[3rem]"/>
+              <span className="nav-item-name mt-[3rem] hidden ">HOME</span>{" "}
+            </Link> 
+            <Link to="/shop" className="flex items-center transition-transform transform hover:translate-x-2">
+              <AiOutlineShopping size={26} className="mr-2 mt-[3rem]"/>
+              <span className="nav-item-name mt-[3rem] hidden ">Shop</span>{" "}
+            </Link> 
+            <Link to="/cart" className="flex items-center transition-transform transform hover:translate-x-2">
+              <AiOutlineShoppingCart size={26} className="mr-2 mt-[3rem]"/>
+              <span className="nav-item-name mt-[3rem] hidden ">Cart</span>{" "}
+            </Link>
+            <Link to="/favorite" className="flex items-center transition-transform transform hover:translate-x-2">
+              <FaHeart size={26} className="mr-2 mt-[3rem]"/>
+              <span className="nav-item-name mt-[3rem] hidden ">Favorite</span>{" "}
+            </Link>
+            <button className="flex items-center text-gray-800 focus:outline-none">
+              {userInfo?<span className="text-white">{userInfo.username}</span>:<></>}
+            </button>
+          </div>
+          <ul>
+           <li>
+              
+            <Link to="/login" className="flex items-center transition-transform transform hover:translate-x-2">
+              <AiOutlineLogin size={26} className="mr-2 mt-[3rem]"/>
+              <span className="nav-item-name mt-[3rem] hidden ">Login</span>{" "}
+            </Link>
+            </li>
+            <li>
+              
+            <Link to="/register" className="flex items-center transition-transform transform hover:translate-x-2">
+              <AiOutlineUserAdd size={26} className="mr-2 mt-[3rem]"/>
+              <span className="nav-item-name mt-[3rem] hidden ">Register</span>{" "}
+            </Link>
+            </li>
+          </ul>
+    </div>
   )
 }
 
