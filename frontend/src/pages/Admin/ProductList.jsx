df0cb73474305982f9687fc813e6d81ce3aa5b1c
adilalishahg <adilalishahg@gmail.com>
2024-04-06 18:17:54 -0700

    [frontend][Product][Working]

---
+++ frontend/src/pages/Admin/ProductList.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import { useNavigate } from "react-router"
+import { useCreateProductMutation, useUploadProductImageMutation } from "../../redux/api/productApiSlice"
+import { useFetchCategoriesQuery } from "../../redux/api/categoryApiSlice"
+
+ 
+const ProductList = () => {
+  return (
+    <div>ProductList</div>
+  )
+}
+
+export default ProductList
